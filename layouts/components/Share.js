152f1b8283498a715f06ff3f345c3e94f57f9b5b
ltxstudio <ltxstudio.info@gmail.com>
2024-11-15 21:34:36 +0600

    Update Share.js

--- layouts/components/Share.js
+++ layouts/components/Share.js
@@ -11,46 +11,47 @@ const Share = ({ title, description, slug, className }) => {
   const { base_url } = config.site;
 
   return (
-    <ul className={`${className}`}>
-      <li className="inline-block">
+    <ul className={`flex space-x-4 ${className}`}>
+      <li>
         <a
           aria-label="facebook share button"
           href={`https://facebook.com/sharer/sharer.php?u=${base_url}/${slug}`}
           target="_blank"
           rel="noreferrer noopener"
-          button="true"
+          className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white hover:bg-blue-700 transition duration-300"
         >
           <IoLogoFacebook />
         </a>
       </li>
-      <li className="inline-block">
+      <li>
         <a
           aria-label="twitter share button"
-          href={`https://twitter.com/intent/tweet/?text=${title}&amp;url=${base_url}/${slug}`}
+          href={`https://twitter.com/intent/tweet/?text=${title}&url=${base_url}/${slug}`}
           target="_blank"
           rel="noreferrer noopener"
-          button="true"
+          className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-400 text-white hover:bg-blue-500 transition duration-300"
         >
           <IoLogoTwitter />
         </a>
       </li>
-      <li className="inline-block">
+      <li>
         <a
           aria-label="linkedin share button"
           href={`https://www.linkedin.com/shareArticle?mini=true&url=${base_url}/${slug}&title=${title}&summary=${description}&source=${base_url}`}
           target="_blank"
           rel="noreferrer noopener"
+          className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-800 text-white hover:bg-blue-900 transition duration-300"
         >
           <IoLogoLinkedin />
         </a>
       </li>
-      <li className="inline-block">
+      <li>
         <a
           aria-label="pinterest share button"
           href={`https://pinterest.com/pin/create/button/?url=${base_url}/${slug}&media=&description=${description}`}
           target="_blank"
           rel="noreferrer noopener"
-          button="true"
+          className="flex items-center justify-center w-10 h-10 rounded-full bg-red-600 text-white hover:bg-red-700 transition duration-300"
         >
           <IoLogoPinterest />
         </a>
